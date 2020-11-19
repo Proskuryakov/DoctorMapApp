73305c0a8c25913068d658f3865a44e10ed8d413
Egor Proskuryakov <proskur.egor@yandex.ru>
2020-11-19 11:53:23 +0300

    Initial commit

---
+++ src/main/java/ru/vsu/cs/app/DoctorMapAppApplication.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorMapAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoctorMapAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vsu/cs/app/DoctorMapAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorMapAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
