1ce90bd47d29267d776c77dea74203e07cad4c8e
Egor Proskuryakov <proskur.egor@yandex.ru>
2020-11-20 13:58:45 +0300

    Add user mapper

---
+++ services/src/main/java/ru/vsu/cs/app/services/mappers/UserMapper.java
@@ -0,0 +1,28 @@
+package ru.vsu.cs.app.services.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.stereotype.Service;
+import ru.vsu.cs.app.db.models.UserModel;
+import ru.vsu.cs.app.services.models.User;
+
+@Service
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    @Mapping(target = "id", source = "user.id")
+    @Mapping(target = "email", source = "user.email")
+    @Mapping(target = "password", source = "encodePassword")
+    @Mapping(target = "fio", source = "user.fio")
+//    @Mapping(target = "userRole.name", source = "user.role")
+//    @Mapping(target = "userRole.id", source = "user.role.id")
+    UserModel toModel(User user, String encodePassword);
+
+    @Mapping(target = "id", source = "userModel.id")
+    @Mapping(target = "email", source = "userModel.email")
+    @Mapping(target = "password", source = "userModel.password")
+    @Mapping(target = "fio", source = "userModel.fio")
+    @Mapping(target = "role", source = "userModel.userRole.name")
+    User fromModel(UserModel userModel);
+
+}
