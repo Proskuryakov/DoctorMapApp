7f2c3af814973a9f913069b2400b9090f371bcd8
Egor Proskuryakov <proskur.egor@yandex.ru>
2020-11-20 13:57:12 +0300

    Connect db and setting migration

---
+++ db/src/main/resources/migrations/1.0/DOCOTOR_MAP_APP-1-20201117-1730.sql
@@ -0,0 +1,25 @@
+create table user_role
+(
+    id   integer primary key not null,
+    name varchar(30)         not null
+);
+
+create table users
+(
+    id       SERIAL primary key,
+    email    varchar(100) not null unique,
+    password char(512)    not null,
+    fio      varchar(250) not null,
+    role_id  integer      not null,
+
+    constraint users_user_role_role_id_fk
+        foreign key (role_id)
+            references user_role (id)
+);
+
+insert into user_role
+values (1, 'ADMIN'),
+       (2, 'USER');
+
+insert into users (email, password, fio, role_id)
+values ('admin@mail.ru', 'admin', 'Иванов Иван Иванович', 1);
