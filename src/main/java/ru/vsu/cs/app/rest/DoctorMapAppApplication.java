0067bfaeeddf401174ff157f33145a267c0faafd
Egor Proskuryakov <proskur.egor@yandex.ru>
2020-11-20 14:02:13 +0300

    Moved

--- src/main/java/ru/vsu/cs/app/rest/DoctorMapAppApplication.java
+++ src/main/java/ru/vsu/cs/app/rest/DoctorMapAppApplication.java
@@ -1,9 +1,12 @@
-package ru.vsu.cs.app;
+package ru.vsu.cs.app.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import ru.vsu.cs.app.services.ServiceConfig;
 
 @SpringBootApplication
+@Import(ServiceConfig.class)
 public class DoctorMapAppApplication {
 
     public static void main(String[] args) {
